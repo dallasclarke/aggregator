8c70d1d326ea2292a80f3f59a29e731ec532f40a
Dallas Clarke <dallas.ag.clarke@gmail.com>
2023-10-31 21:38:39 -0700

    Setting up chi and working on json function

--- main.go
+++ main.go
@@ -4,8 +4,11 @@ import (
 	"fmt"
 	"log"
 	"os"
+  "net/http"
 
   "github.com/joho/godotenv"
+  "github.com/go-chi/chi"
+  "github.com/go-chi/cors"
 )
 
 func main() {
@@ -15,5 +18,27 @@ func main() {
   if portString == "" {
     log.Fatal("PORT is not found in .env file")
   }
+
+  router := chi.NewRouter()
+
+  router.Use(cors.Handler(cors.Options{
+    AllowedOrigins: []string{"https://*", "http://*"},
+    AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+    AllowedHeaders: []string{"*"},
+    ExposedHeaders: []string{"Link"},
+    MaxAge: 300,
+  }))
+
+  srv := &http.Server{
+    Handler: router,
+    Addr: ":" + portString,
+  }
+
+  log.Printf("Server starting on port %v", portString)
+  err := srv.ListenAndServe()
+  if err != nil {
+    log.Fatal(err)
+  }
+
   fmt.Println("PORT: ", portString)
 }
